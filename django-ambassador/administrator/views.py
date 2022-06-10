4f09a959a68865340a3e3b63e9fcc44e6c60335a
Julio Felipe <julio@informach.com.br>
2022-06-10 19:18:34 -0300

    feat: add order and product api

--- django-ambassador/administrator/views.py
+++ django-ambassador/administrator/views.py
@@ -1,3 +1,4 @@
+from administrator.serializers import LinkSerializer, OrderSerializer, ProductSerializer
 from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -6,7 +7,7 @@ from rest_framework.views import APIView
 from common.authentication import JWTAuthentication
 from common.serializers import UserSerializer
 
-from core.models import User
+from core.models import Link, Order, Product, User
 
 class AmbassadorAPIView(APIView):
     authentication_classes = [JWTAuthentication]
@@ -15,4 +16,49 @@ class AmbassadorAPIView(APIView):
     def get(self, _):
         ambassadors = User.objects.filter(is_ambassador=True)
         serializer = UserSerializer(ambassadors, many=True)
+        return Response(serializer.data)
+
+
+class ProductGenericAPIView(
+    generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.CreateModelMixin,
+    mixins.UpdateModelMixin, mixins.DestroyModelMixin
+):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, pk=None):
+        if pk:
+            return self.retrieve(request, pk)
+
+        return self.list(request)
+
+    def post(self, request):
+       return self.create(request)
+
+    def put(self, request, pk=None):
+        return self.partial_update(request, pk)
+
+    def delete(self, request, pk=None):
+        return self.destroy(request, pk)
+
+
+class LinkAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk=None):
+        links = Link.objects.filter(user_id=pk)
+        serializer = LinkSerializer(links, many=True)
+        return Response(serializer.data)
+
+
+class OrderAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        orders = Order.objects.filter(complete=True)
+        serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
